c4a82d8134b8ff147ffc33f957ede2bc26cc575f
MuYang <365312137@qq.com>
2020-01-09 00:15:20 +0800

    修改统一异常处理

--- tensquare_spit/src/main/java/com/tensquares/spit/controller/BaseExceptionHandler.java
+++ tensquare_spit/src/main/java/com/tensquares/spit/controller/BaseExceptionHandler.java
@@ -2,6 +2,7 @@ package com.tensquares.spit.controller;
 
 import entity.Result;
 import entity.StatusCode;
+
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
